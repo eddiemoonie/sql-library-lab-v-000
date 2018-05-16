01b80091b58a3003d9b2e204d533257d729f01c2
Eddie Moon <eddiemoonie@gmail.com>
2018-05-16 21:06:26 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -42,5 +42,5 @@ def select_character_names_and_number_of_books_they_are_in
    LEFT JOIN characters
    ON character_books.character_id = characters.id
    GROUP BY characters.name
-   ORDER BY book_count"
+   ORDER BY book_count DESC"
 end
